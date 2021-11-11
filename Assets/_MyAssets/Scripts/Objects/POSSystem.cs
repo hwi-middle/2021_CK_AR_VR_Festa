54bd9f3bd567046676d699ab9acd22a67c0c56ec
Ju Hwijung <gnlwnd5084@gmail.com>
2021-11-12 01:21:45 +0900

    더미용 맵 적용, 포스기 버튼 구현

--- Assets/_MyAssets/Scripts/Objects/POSSystem.cs
+++ Assets/_MyAssets/Scripts/Objects/POSSystem.cs
@@ -10,13 +10,16 @@ public class POSSystem : MonoBehaviour
     private List<Goods> _goodsList = new List<Goods>();
     private List<int> _goodsCount = new List<int>();
     private AudioSource _audioSource;
+    private int _paidAmount = 0;
+    private string _paidAmountString = "";
 
     [SerializeField] private GameObject[] posRows; //포스기 상품정보에서 한 줄에 출력되는 텍스트들의 부모 오브젝트
     [SerializeField] private Text totalText; //합계 금액이 출력되는 텍스트
+    [SerializeField] private Text paidText; //낸 금액이 출력되는 텍스트
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {
         _audioSource = GetComponent<AudioSource>();
     }
 
@@ -77,6 +80,8 @@ public class POSSystem : MonoBehaviour
             posRows[i].transform.GetChild(2).GetComponent<Text>().text = "";
             posRows[i].transform.GetChild(3).GetComponent<Text>().text = "";
         }
+
+        _totalPrice = 0;
     }
 
     private void Refresh()
@@ -91,5 +96,49 @@ public class POSSystem : MonoBehaviour
         }
 
         totalText.text = "₩" + $"{_totalPrice:n0}";
+        paidText.text = "₩" + _paidAmountString;
+    }
+
+    public void InputPosButton(string key)
+    {
+        switch (key)
+        {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+            case "00":
+                if (_paidAmountString.Length >= 10) break;
+                _paidAmountString = key;
+                break;
+            case "backspace":
+                _paidAmountString = _paidAmountString.Substring(_paidAmountString.Length - 1);
+                break;
+            case "reset": //리셋
+                if (_paidAmountString == "")
+                {
+                    ResetGoods();
+                }
+                else
+                {
+                    _paidAmountString = "";
+                }
+
+                break;
+            case "accept": //승인
+            case "apply": //확인
+                break;
+            default:
+                Debug.Assert(false);
+                break;
+        }
+
+        Refresh();
     }
 }
