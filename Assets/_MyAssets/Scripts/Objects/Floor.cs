36115c75f49258799ab96086f4c5f67dd506fa76
Ju Hwijung <gnlwnd5084@gmail.com>
2021-11-23 01:14:54 +0900

    오브젝트가 바닥과 충돌시 리스폰되는 시스템 구현

---
+++ Assets/_MyAssets/Scripts/Objects/Floor.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    [SerializeField] private GameObject respawnSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Cash":
+            case "Goods":
+                other.gameObject.transform.position = respawnSpot.transform.position;
+                break;
+        }
+    }
+}
